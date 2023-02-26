a6e3747e10729f6eb73b21b46553f0bde609ecc4
Анастасия Сечко <asanastasia1@gmail.com>
2023-02-26 14:49:43 +0300

    mess

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi everyone!");
+        System.out.println("My name is Anastasiya Golubeva.");
+        System.out.println("I am a manual QA.");
+        System.out.println("I collect nested dolls=)");
+    }
+}
